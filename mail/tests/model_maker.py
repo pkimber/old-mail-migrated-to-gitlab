e0dbef905254e324732e51fd3fabb50b6bc90b21
Patrick Kimber <code@pkimber.net>
2014-08-02 11:16:52 +0100

    move notify model from 'enquiry' to 'mail'

---
+++ mail/tests/model_maker.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+from base.tests.model_maker import clean_and_save
+
+from mail.models import Notify
+
+
+def make_notify(email, **kwargs):
+    defaults = dict(
+        email=email,
+    )
+    defaults.update(kwargs)
+    return clean_and_save(Notify(**defaults))
