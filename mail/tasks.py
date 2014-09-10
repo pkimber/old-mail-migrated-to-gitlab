78936f28f5770bed64e5fdccba6bbd807306e0ce
Patrick Kimber <code@pkimber.net>
2014-09-10 19:35:25 +0100

    mail - send using celery

--- mail/tasks.py
+++ mail/tasks.py
@@ -1,11 +1,16 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
 
-from celery import task
+from celery import shared_task
 
 from .service import send_mail
 
 
-@task()
+@shared_task
 def process_mail():
+    send_mail()
+
+
+@shared_task
+def process_periodic_task():
     send_mail()
