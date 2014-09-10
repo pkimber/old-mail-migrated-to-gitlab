9beeceab93a1eae413fb5a789b9deebbbd99c276
Patrick Kimber <code@pkimber.net>
2014-09-10 19:35:36 +0100

    version 0.1.16

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/mail/*.*',
         ],
     },
-    version='0.1.15',
+    version='0.1.16',
     description='mail',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
