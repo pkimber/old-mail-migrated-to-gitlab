7d458bddb1a7e62b98cd0be70ca89a4283666733
Patrick Kimber <code@pkimber.net>
2014-11-28 16:01:52 +0100

    version 0.1.20

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/mail/*.*',
         ],
     },
-    version='0.1.19',
+    version='0.1.20',
     description='mail',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
