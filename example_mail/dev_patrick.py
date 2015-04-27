4bf8b83afac56428c831a91cd23490c075fedebc
Patrick Kimber <code@pkimber.net>
2015-04-27 19:32:43 +0100

    tidy

--- example_mail/dev_patrick.py
+++ example_mail/dev_patrick.py
@@ -1,13 +1,11 @@
 # -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
-
 from .base import *
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test_mail_patrick',
+        'NAME': 'test_app_mail_patrick',
         'USER': 'patrick',
         'PASSWORD': '',
         'HOST': '',
