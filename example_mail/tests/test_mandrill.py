dc94457284114d2f6f7cec4fb5ccb61ae8c9e9a9
Patrick Kimber <code@pkimber.net>
2016-02-04 23:57:14 +0000

    test 'retry_count' #1325

--- example_mail/tests/test_mandrill.py
+++ example_mail/tests/test_mandrill.py
@@ -3,17 +3,44 @@ import pytest
 
 from unittest import mock
 
-from mail.models import MailError
-from mail.service import _send_mail_mandrill_template
+from mail.models import (
+    MailError,
+    MailTemplate,
+)
+from mail.service import (
+    _mail_send,
+    _send_mail_mandrill_template,
+)
 from mail.tests.factories import (
     MailFactory,
+    MailTemplateFactory,
     MessageFactory,
 )
 from .factories import EnquiryFactory
 
 
 @pytest.mark.django_db
-def test_send_mail_mandrill_template(client, mocker):
+def test_mail_send(client):
+    with mock.patch('django.core.mail.EmailMultiAlternatives') as mock_mail:
+        mock_mail.return_value.mandrill_response = [{
+            "email": "abc@test.com",
+            "status": "rejected",
+            "_id": "123",
+            "reject_reason": "hard-bounce"
+        }]
+        template = MailTemplateFactory(template_type=MailTemplate.MANDRILL)
+        message = MessageFactory(
+            template=template,
+            content_object=EnquiryFactory(),
+        )
+        obj = MailFactory(message=message)
+        _mail_send([obj.pk])
+        obj.refresh_from_db()
+        assert 1 == obj.retry_count
+
+
+@pytest.mark.django_db
+def test_send_mail_mandrill_template(client):
     with mock.patch('django.core.mail.EmailMultiAlternatives') as mock_mail:
         mock_mail.return_value.mandrill_response = [{
             "email": "abc@test.com",
