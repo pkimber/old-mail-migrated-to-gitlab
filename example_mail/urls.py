3e386ae5b6616e956f46f93fef3349d1cdb8ec5e
Patrick Kimber <code@pkimber.net>
2016-08-23 17:17:10 +0100

    Update to Django 1.10

--- example_mail/urls.py
+++ example_mail/urls.py
@@ -1,12 +1,6 @@
 # -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.conf import settings
-from django.conf.urls import (
-    include,
-    patterns,
-    url,
-)
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -27,8 +21,7 @@ from .views import (
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(regex=r'^$',
         view=HomeView.as_view(),
         name='project.home'
@@ -58,7 +51,7 @@ urlpatterns = patterns(
         view=SettingsView.as_view(),
         name='project.settings'
         ),
-)
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #   ^ helper function to return a URL pattern for serving files in debug mode.
